1d49884396800d0f4056f2c6bfe7bcd111432bc9
Joseph Zebaze <32811187+strikerjoseph@users.noreply.github.com>
2024-10-11 09:17:03 +0000

    Add files via upload

---
+++ LOOP.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 10 13:57:16 2024
+
+@author: PAVILION
+"""
+
+### loop ####
+
+## "for" loop##
+
+# for i in range(21):
+#     print(i,end=',')
+# for i in range(0,124,4):
+#     print(i,end=',') 
+
+# word= 'blackcat'
+# for i in word:
+#     print(i,end=',')
+
+
+data=[53,76,25,98,56,42,69,81]
+type (data)
+
+data[0]
+# black=()
+# type (black)
+for i in data:
+    print (i, end=' ')
